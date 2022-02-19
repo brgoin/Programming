3333fc5231a041948e58e1f037d794777bf4b0d9
brgoin <99919592+brgoin@users.noreply.github.com>
2022-02-19 10:39:07 -0600

    Add files via upload

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -5,25 +5,32 @@
 # Creation Date: 2/18/2022
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 #####Missing the parentheses at the end of line 6, after them)
+##Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify
+## the issues and correct them.)
 import random
 import time
 
-def displayintro():  ## Intro should be lowecase, referred to as LC from herein
+def displayIntro():
+	## Intro should be lowecase, referred to as LC from herein
+	## Should be  displayintro
 	print('''You are in a land full of dragons. In front of you,
 	you see two caves. In one cave, the dragon is friendly
 	and will share his treasure with you. The other dragon
 	is greedy and hungry, and will eat you on sight.''')
 	print()
 
-def choosecave(): ##LC
+def chooseCave():
+	##Should be  choosecave
 	cave = ()
 	while cave != '1' and cave != '2':
 		print('Which cave will you go into? (1 or 2)')
 		cave = input()
 
-	return (cave)  #### missing parentheses, caves should be singular
+	return cave
+	#### missing parentheses, caves should be singular,  return (cave)
 
-def checkcave(chosencave): ##LC cave
+def checkcave(chosenCave):
+	##LC cave,  chosencave
 	print('You approach the cave...')
 	#sleep for 2 seconds
 	time.sleep(2)
@@ -34,21 +41,26 @@ def checkcave(chosencave): ##LC cave
 	print()
 	#sleep for 2 seconds
 	time.sleep(2)
-	friendlycave = random.randint(1, 2)  ##LC cave
+	friendlyCave = random.randint(1, 2)
+	##LC cave, friendlycave
 
 	if chosen cave == str(friendlycave): ## LC cave and space
 		print('Gives you his treasure!')
 	else:
 		print ('Gobbles you down in one bite!')  ##Missing parenthases ('Gobbles you down in one bite!')
 
-playagain = 'yes' ##LC
-while playagain = 'yes' or playagain = 'y':  ##Can't have a single variable with two different definitions, missing
-	displayintro()      ## the colon after while:  Line 45  ## LC again in line 45 and intro in 46
-	caveNumber = choosen cave:()  ##Should be chosen cave:()  missing space
+playagain = 'Yes'
+while playagain = 'yes' or playagain = 'Y':
+	##Can't have a single variable with two different definitions, missing
+	## the colon after while:  Line 51  ## LC again in line 51   while: 'yes' or playagain = 'y':
+	displayintro()
+	caveNumber = choosencave:()
+	##Should be chosen cave:()  missing space
 	checkCave(caveNumber)
     
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
 	if playAgain == "no":
-		print("Thanks for playing")   ## should be playing not planing
+		print("Thanks for planing")
+		## should be playing not planing  ("Thanks for playing")
 
