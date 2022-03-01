5e4039f0e18f1b7c582842a5a172a3c77e73959e
brgoin <99919592+brgoin@users.noreply.github.com>
2022-03-01 00:31:31 -0600

    Add files via upload

--- Module8Assignment.py
+++ Module8Assignment.py
@@ -1,53 +1,28 @@
 import os
 
-print(
-    'Hi, I can save a new file with your name, address, and phone number, in a specified directory for you, if it is new we will make the directory.')
+# Get necessary input
+directory = input("Specify a directory: ")
+filename = input("Specify a filename: ")
+name = input("Name: ")
+addr = input("Address: ")
+phone = input("Phone: ")
 
-path = input('Please provide directory name: ')
-print(f'The directory name is: {name}')
-directoryName = new directoryName
+# Make a CSV line
+line = f"{name},{addr},{phone}"
 
-fileName = input('What is the new file name?\n)')
-print(f'The file name is:')
-filePath = os.path.join('userPath, newFile')
-with open(filepath, 'w+') as fp:
+# If the directory doesn't exist, create it
+if (os.path.isdir(directory) == False):
+    os.mkdir(directory)
 
-name = input("What is your name?\n)")
-address = input('What is your address?\n)')
-number = input('What is your phone number?\n')
-##Create new file
-try:
-    with open(filePath, 'w') as file_object:
-        data = (name + "," + address + "," + number)
-        file_object.write(data)
-except:
-    print('Error creating/writing to new file.')
-    quit()
-try:
-    with open(filePath) as file_object:
-        print('New file created,' + 'newFile' + ', and added the following information:')
-        print(file_object.read())
-
-def get_name():
-  print = ('What is your name?: ')
-  userName = input('My name is:')
-  print('Hello', name)
-
-print(f'What is your address? ')
-address = input('Your address is: ')
-print(f'What is your phone number? ')
-phoneNum = input('Your phone number is: ')
-print(f'{name}, {add}, {phone}')
-
+# Create a complete file path from the directory and filename
 filePath = os.path.join(directory, filename)
 
-print(f'If it is new enter the name of the new directory')
-input('Name the new directory: ')
-print(f' {new}')
-##Naming the File for the Directory
-file = input("The name of the file for the directory: ")
-print(f'{file}')
-except:
-print(('Error reading file.'))
-
-main()
+# Open the file and write the CSV line to it, and close the file
+with open(filePath, "a") as fh:
+    fh.write(f"{line}\n")
+# I'll leave the last part for you to put together (reading the line from the file)
+def readFile(filename):
+  with open(filename, "r") as fh:
+    print("")
+    print("-"*6+"File Contents"+"-"*6)
+    print(fh.read())
